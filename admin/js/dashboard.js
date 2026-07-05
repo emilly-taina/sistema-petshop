9acf55e68c26b04bae3ca790e0e9c09683a7d5f0
RSM23191 <rsm23@aluno.ifnmg.edu.br>
2026-07-05 09:28:40 -0300

    Update dashboard.js

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -1,1 +1,13 @@
+if(localStorage.getItem("logado")!=="true"){
 
+    window.location.href="login.html";
+
+}
+
+document.getElementById("btnSair").addEventListener("click",()=>{
+
+    localStorage.removeItem("logado");
+
+    window.location.href="login.html";
+
+});
